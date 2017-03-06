3f4ee1e8b8c5c1796a36cc8796e65027af43c9d7
Aysegul Yonet <aysegulyonet@gmail.com>
2017-03-06 12:55:07 -0800

    Unique-string

--- src/app/algorithms/ArraysStrings/unique-char.ts
+++ src/app/algorithms/ArraysStrings/unique-char.ts
@@ -1,4 +1,34 @@
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structures?
-export class UniqueChar {
+// function uniqueChar(s) {
+//   const e = {};
+//   const c = s.split('');
+//   let r = true;
+//
+//   c.forEach(function(l){
+//     if(e[l]) {
+//       r = false;
+//       return;
+//     } else {
+//       e[l]= true;
+//     }
+//   })
+//
+//   return r;
+//
+// };
+
+function uniqueChar(s) {
+  for(let i = 0; i < s.length; i++) {
+    let c = s[i];
+
+    let sub = s.substring(i + 1);
+    console.log(sub, c,sub.indexOf(c));
+    if(sub.indexOf(c) > -1) return false;
+  }
+  return true;
 }
+
+
+uniqueChar('aysegul');
+uniqueChar('aysegulss');
